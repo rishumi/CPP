39322ea9234e2d257ce6cf3d1c2e7f49f20576fc
rishumi <rishumishra105@gmail.com>
2025-03-23 17:20:58 +0530

    input-output

---
+++ ifElse.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int age;
+    cin>> age;
+    if(age >= 18) {
+        cout<< "He is an adult";
+    } else
+    {
+        cout<< "He is not an adult";
+    }
+    return 0;
+}
